27f7a062d97983ac07aff61dc5add2c132212c93
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-11 13:16:54 +0200

    session code gekopieerd en bewerkt van oud project

---
+++ Session.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+
+$email = $_SESSION['email'];
+$select = "SELECT * from users where email='".$email."'";
+$result = mysqli_query($conn,$select)  or die( mysqli_error($conn));
+$row = mysqli_fetch_array($result);
+$_SESSION['role'] = $row['role'];
+$_SESSION['firstname'] = $row['firstname'];
+$_SESSION['id'] = $row['id'];
+?>
