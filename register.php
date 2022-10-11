87fd9f89f650575d93e3f322b60b6feb5c7f193c
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-11 13:47:35 +0200

    Register klaar

--- register.php
+++ register.php
@@ -1,6 +1,30 @@
 <?php 
 require "connection.php";
 
+if(isset($_POST["submit"]) && $_POST["firstname"] != "" && $_POST['lastname'] != "" && $_POST['email'] != "" && $_POST['password'] != "")
+{
+    
+$firstname = $_POST['firstname']; 
+$lastname = $_POST['lastname']; 
+$email = $_POST['email']; 
+$password = $_POST['password']; 
+$date_of_birth = $_POST['date_of_birth']; 
+$phonenumber = $_POST['phonenumber']; 
+$address = $_POST['address']; 
+$zipcode = $_POST['zipcode']; 
+$city = $_POST['city']; 
+$role = $_POST['role']; 
+
+$sql = "INSERT INTO users (firstname, lastname, email, password, date_of_birth, phonenumber, address, zipcode, city, role)
+VALUES ('$firstname', '$lastname', '$email','$password','$date_of_birth','$phonenumber','$address','$zipcode','$city','Klant')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    header("location: index.php");
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }$conn->close();
+}
 ?>
 
 
@@ -20,12 +44,11 @@ require "connection.php";
         <input type="text" name="email" id="email" placeholder="email">
         <input type="password" name="password" id="password" placeholder="">
         <input type="date" name="date_of_birth" id="date_of_birth" placeholder="">
-        <input type="text" name="phonenumber" id="phonenumber" placeholder="">
+        <input type="text" name="phonenumber" id="phonenumber" placeholder="06123456789">
         <input type="text" name="address" id="address" placeholder="address">
         <input type="text" name="zipcode" id="zipcode" placeholder="zipcode">
         <input type="text" name="city" id="city" placeholder="city">
-        <input type="hidden" name="role" id="role" value="Klant">
-        <button type="submit">register</button>
+        <button type="submit" name="submit">register</button>
     </form>
 </body>
 </html>
