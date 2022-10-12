1c9e436441cfd74a44a0f949a2110e723f053e39
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-12 10:46:28 +0200

    grid

--- index.php
+++ index.php
@@ -13,16 +13,17 @@ include "session.php";
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <a href="login.php">login</a>
-    <a href="register.php">register</a>
-    <a href="logout.php">logout</a>
-    <?php if(isset($_SESSION['id'])){?>
-        <a href="edit_user.php?id=<?php echo $_SESSION['id'] ?>"><?php echo $_SESSION['firstname']; ?></a>
-    <?php } ?>
+    
 
     <div class="grid-container">
         <div class="grid-item grid-item1">a</div>
-        <div class="grid-item grid-item2">b</div>
+        <div class="grid-item grid-item2">
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="logout.php">logout</a>
+        <?php if(isset($_SESSION['id'])){?>
+            <a href="edit_user.php?id=<?php echo $_SESSION['id'] ?>"><?php echo $_SESSION['firstname']; ?></a>
+        <?php } ?></div>
         <div class="grid-item grid-item3">c</div>
         <div class="grid-item grid-item4">d</div>
         <div class="grid-item grid-item5">e</div>
