edef1bfae47b8ebdaf0859763fb0aaf08b386e42
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-12 08:39:03 +0200

    logout

--- Session.php
+++ Session.php
@@ -1,10 +1,12 @@
 <?php 
 session_start();
 
-$email = $_SESSION['email'];
-$select = "SELECT * from users where email='".$email."'";
-$result = mysqli_query($conn,$select)  or die( mysqli_error($conn));
-$row = mysqli_fetch_array($result);
-$_SESSION['role'] = $row['role'];
-$_SESSION['firstname'] = $row['firstname'];
-$_SESSION['id'] = $row['id'];
+if($_SESSION['id'] !== ''){
+    $id = $_SESSION['id'];
+    $select = "SELECT * from users where id='".$id."'";
+    $result = mysqli_query($conn,$select)  or die( mysqli_error($conn));
+    $row = mysqli_fetch_array($result);
+    $_SESSION['role'] = $row['role'];
+    $_SESSION['firstname'] = $row['firstname'];
+    $_SESSION['id'] = $row['id'];
+}
