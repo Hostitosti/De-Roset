a75489085a16d2f334006ee21ca2e6a3e6722b41
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-13 10:15:20 +0200

    titels

--- winkelmandje.php
+++ winkelmandje.php
@@ -11,7 +11,7 @@ include "session.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>blog</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
