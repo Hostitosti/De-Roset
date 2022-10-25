1d8b2e056c7ea6eb6a580eef19a687f10c70e5a7
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-25 09:17:50 +0200

    bootstrap

--- worker_page.php
+++ worker_page.php
@@ -13,6 +13,7 @@ include "session.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medewerker pagina</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
     <div class="grid-container">
@@ -49,7 +50,9 @@ include "session.php";
             <a href="#">Bestel</a>
         </div>
                 
-        <div class="grid-item grid-item4">d</div>
+        <div class="grid-item grid-item4">
+                
+        </div>
         
         <div class="grid-item grid-item5">
             <h2>Populaire smaken</h2>
